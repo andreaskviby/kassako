ae659ab8a0983b889bc81b7c0acc63131e268f4a
Andreas Kviby <andreaskviby@GOOSE-4.local>
2025-12-29 22:07:24 +0100

    Test with minimal scope (companyinformation only)

--- config/fortnox.php
+++ config/fortnox.php
@@ -4,7 +4,7 @@ return [
     'client_id' => env('FORTNOX_CLIENT_ID'),
     'client_secret' => env('FORTNOX_CLIENT_SECRET'),
     'redirect_uri' => env('FORTNOX_REDIRECT_URI'),
-    'scopes' => env('FORTNOX_SCOPES', 'companyinformation invoice supplier order customer bookkeeping'),
+    'scopes' => env('FORTNOX_SCOPES', 'companyinformation'),
     'webhook_secret' => env('FORTNOX_WEBHOOK_SECRET'),
 
     // Fortnox uses the same endpoints for production and test environments
